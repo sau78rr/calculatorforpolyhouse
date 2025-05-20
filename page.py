892ea5c3fc912f234a202ac93a67355cdfaa374a
sau78rr <133512921+sau78rr@users.noreply.github.com>
2025-05-20 13:52:02 +0530

    Update page.py

--- page.py
+++ page.py
@@ -14,6 +14,7 @@ if polyhouse_type == 'NVPH':
     domes_list = []
     no_of_steps = None
     isstepper=False
+    single_steps=0
     # Only show stepper fields if chosen
     if type_of_structure == 'Stepper':
         no_of_steps = st.number_input('Number of steps:', min_value=0, step=1, value=1)
